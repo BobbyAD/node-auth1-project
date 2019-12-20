3bd40320afefa23978dee88df64a89f6e21b4eab
Robert Driskell <driskell.ba@gmail.com>
2019-12-19 20:50:33 -0600

    sessions stored

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -2,19 +2,31 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+
+const knex = require("../database/dbConfig.js");
 
 const isProd =
     process.env.NODE_ENV && process.env.NODE_ENV !== "development"
         ? true
         : false;
 
+const sessionExpire = 1000 * 60 * 10;
+
 const sessionConfiguration = {
     name: "test_auth_session",
     secret: process.env.SESSION_SECRET,
     saveUninitialized: !isProd,
     resave: false,
+    store: new KnexSessionStore({
+        knex,
+        createtable: true,
+        clearInterval: sessionExpire,
+        tablename: "sessions",
+        sidfieldname: "id",
+    }),
     cookie: {
-        maxAge: 1000 * 60 * 10,
+        maxAge: sessionExpire,
         secure: isProd,
         httpOnly: true
     }
