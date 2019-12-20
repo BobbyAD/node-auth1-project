24fcdcfbad8b7469080b3f08d9826f14d002aa73
Robert Driskell <driskell.ba@gmail.com>
2019-12-19 20:59:57 -0600

    logout added

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -14,7 +14,7 @@ router.post("/register", (req, res) => {
 
     Users.add(user)
         .then(saved => {
-            req.session.user = user
+            req.session.user = user;
             res.status(201).json(saved);
         })
         .catch(error => {
@@ -25,15 +25,13 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
     let { username, password } = req.body;
 
-    
-
     Users.findBy({ username })
         .first()
         .then(user => {
             // check that the password is valid
             if (user && bcrypt.compareSync(password, user.password)) {
                 // save a session
-                req.session.user = user
+                req.session.user = user;
                 res.status(200).json({ message: `Welcome ${user.username}!` });
             } else {
                 res.status(401).json({ message: "Invalid Credentials" });
@@ -42,6 +40,20 @@ router.post("/login", (req, res) => {
         .catch(error => {
             res.status(500).json(error);
         });
+});
+
+router.get("/logout", (req, res) => {
+    if (req.session) {
+        req.session.destroy(error => {
+            if (error) {
+                res.status(500).json({ message: "Error logging out" });
+            } else {
+                res.status(200).json({ message: "Logged out successfully" });
+            }
+        });
+    } else {
+        res.status(400).json({ message: "Not logged in" });
+    }
 });
 
 module.exports = router;
